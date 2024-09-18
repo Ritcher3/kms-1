f6973589f541f58846feb5327d5f9f7ad23c6976
Ritcher <ritcherstpreux@gmail.com>
2024-09-18 16:26:51 -0400

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,13 @@
 output "key_id" {
-  value       = try(aws_kms_key.this[0].key_id, "")
+  value       = try(aws_kms_key.main[0].key_id, "")
   description = "The ID of the KMS key"
 }
 
 output "key_arn" {
-  value       = try(aws_kms_key.this[0].arn, "")
+  value       = try(aws_kms_key.main[0].arn, "")
   description = "The ARN of the KMS key"
+}
+
+output "account_id" {
+  value = local.account_id
 }
